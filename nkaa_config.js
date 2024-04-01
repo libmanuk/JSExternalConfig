80cdaefd6cabc67c95962dbda0d728caa1e15b75
Eric C. Weig <eweig@email.uky.edu>
2024-04-01 15:09:59 -0400

    Create nkaa_config.js

---
+++ nkaa_config.js
@@ -0,0 +1,27 @@
+const nkaa_config = {
+  hdrSimple: "0",
+  hdrInclude: "0",
+  hdrHomeLabel: " ",
+  hdrHomeLink: " ",
+  hdrLink1Label: " ",
+  hdrLink1Url: " ",
+  hdrLink2Label: " ",
+  hdrLink2Url: " ",
+  hdrLink3Label: " ",
+  hdrLink3Url: " ",
+  hdrLink4Label: " ",
+  hdrLink4Url: " ",
+  hdrLink5Label: " ",
+  hdrLink5Url: " ",
+  hdrLink6Label: "",
+  hdrLink6Url: "",
+  hdrSrchInclude: "0",
+  hdrWidth: "80",
+  hdrAlertInclude: "0",
+  hdrAlertTitle: "NKAA Unexpected Service Disruption",
+  hdrAlertMsg: "We are currently working to resolve an authentication issue affecting EBSCO databases.",
+  hdrAlertUrlLabel: "more information",
+  hdrAlertUrl: "https://google.com"
+}
+
+export default nkaa_config;
